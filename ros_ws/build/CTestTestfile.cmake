7c26b27f354c45af5098f1d789f30c9f4988defe
Joshua Frank <josh@tesseractmobile.com>
2014-03-29 19:35:43 -0500

    added ros

---
+++ ros_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/efim/efim/ros_ws/src
+# Build directory: /home/efim/efim/ros_ws/build
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
---
+++ ros_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,4 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/efim/efim/ros_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
