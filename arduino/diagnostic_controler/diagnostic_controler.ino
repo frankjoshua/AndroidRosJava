20bdeec956b4210352bb375859ad4b88bc4b8dc1
frankjoshua <frankjoshua@gmail.com>
2015-06-10 22:20:30 -0500

    wip

--- arduino/diagnostic_controler/diagnostic_controler.ino
+++ arduino/diagnostic_controler/diagnostic_controler.ino
@@ -2,21 +2,32 @@
 #include <EasyTransfer.h>
 #include <Adafruit_NeoPixel.h>
 #include <SoftwareSerial.h>
+#include <Wire.h>
+#include <LiquidTWI.h>
 
 #define NEO_PIN 13
 
 ClientTarget clientTarget;
 SoftwareSerial SWSerial(4, 5);
 
+// Connect via i2c, default address #0 (A0-A2 not jumpered)
+LiquidTWI lcd(0);
+
 void setup() 
 { 
+  lcd.begin(16,2);
+  lcd.print("LCD: OK");
+  lcd.setBacklight(HIGH);
   SWSerial.begin(COM_SPEED);
   clientTarget.begin(NEO_PIN, &SWSerial);
   
   //Register as Listener
+  lcd.setCursor(0,1);
+  lcd.print("Registering");
   clientTarget.registerListener(TARGET_MOTOR_LEFT);
   clientTarget.registerListener(TARGET_MOTOR_RIGHT);
-  
+  lcd.setCursor(0,1);
+  lcd.print("Ready      ");
   
 } 
 
@@ -25,11 +36,14 @@ void loop()
 { 
   delay(50);
   if(clientTarget.receiveData()){
+    lcd.setCursor(0,0);
     mActive = !mActive;
     if(mActive){
       clientTarget.setPixelColor(COLOR_ACTIVE);
+      lcd.print("*              ");
     } else{
       clientTarget.setPixelColor(0,0,0);
+      lcd.print("-              ");
     }
   } else {
     clientTarget.setPixelColor(COLOR_OK);
