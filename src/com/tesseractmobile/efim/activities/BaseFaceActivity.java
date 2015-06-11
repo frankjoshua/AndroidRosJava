7197d03c479d87930a5519b00bb3bce5c8e9e801
frankjoshua <frankjoshua@gmail.com>
2015-06-11 13:36:11 -0500

    Added chatbot sdk

--- src/com/tesseractmobile/efim/activities/BaseFaceActivity.java
+++ src/com/tesseractmobile/efim/activities/BaseFaceActivity.java
@@ -1,10 +1,16 @@
 package com.tesseractmobile.efim.activities;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.WindowManager;
 
+import com.google.code.chatterbotapi.ChatterBot;
+import com.google.code.chatterbotapi.ChatterBotFactory;
+import com.google.code.chatterbotapi.ChatterBotSession;
+import com.google.code.chatterbotapi.ChatterBotType;
 import com.tesseractmobile.efim.R;
 import com.tesseractmobile.efim.views.MouthView;
 
@@ -20,9 +26,13 @@ abstract public class BaseFaceActivity extends Activity implements OnClickListen
 
         mouthView = (MouthView) findViewById(R.id.mouthView);
         
+        //Setup click listeners
         findViewById(R.id.eyeViewLeft).setOnClickListener(this);
         findViewById(R.id.eyeViewRight).setOnClickListener(this);
         findViewById(R.id.mouthView).setOnClickListener(this);
+        
+        //Keep the screen on
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
     }
 
     @Override
@@ -31,18 +41,78 @@ abstract public class BaseFaceActivity extends Activity implements OnClickListen
 
         switch (viewId) {
         case R.id.eyeViewLeft:
-            getMouthView().setText(R.string.better_not_die);
-            break;
-        case R.id.eyeViewRight:
-            getMouthView().setText(R.string.ghost_in_the_machine);
-            break;
-        case R.id.mouthView:
+            getMouthView().setText("Ouch");
             finish();
             break;
+        case R.id.eyeViewRight:
+            getMouthView().setText("I'm going to kill you in my sleep... Oh wait, your sleep");
+            break;
+        case R.id.mouthView:
+            new BotTask().execute();
+            break;  
         }
     }
     
     protected final MouthView getMouthView(){
         return mouthView;
+    }
+    
+    private class BotTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(final Void... params) {
+            try {
+                final ChatterBotFactory factory = new ChatterBotFactory();
+
+                final ChatterBot bot1 = factory.create(ChatterBotType.JABBERWACKY);
+                final ChatterBotSession bot1session = bot1.createSession();
+
+                //final ChatterBot bot2 = factory.create(ChatterBotType.PANDORABOTS, "b0dafd24ee35a477");
+                final ChatterBot bot2 = factory.create(ChatterBotType.JABBERWACKY);
+                final ChatterBotSession bot2session = bot2.createSession();
+
+                String s = "Hello";
+                int count = 2;
+                while (count > 0) {
+                    count--;
+                    
+                    if(s.equals("")){
+                        s = "Are you still there?";
+                    }
+                    final String s2 = s;
+                    runOnUiThread(new Runnable() {
+                        
+                        @Override
+                        public void run() {
+                            
+                            getMouthView().setText(s2);
+                        }
+                    });
+                   
+                    
+                    s = bot1session.think(s);
+                    if(s.equals("")){
+                        s = "Can you hear me?";
+                    }
+                    //s = Integer.toString(count);
+                    final String s3 = s;
+                    runOnUiThread(new Runnable() {
+                        
+                        @Override
+                        public void run() {
+                            
+                            getMouthView().setText(s3);
+                        }
+                    });
+                    s = bot2session.think(s);
+                    
+                }
+                
+            } catch (final Exception e) {
+                // TODO: handle exception
+            }
+            return null;
+        }
+        
     }
 }
