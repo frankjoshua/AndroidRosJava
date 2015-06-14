99050f2a966078f110e607aa92a66658e5c93cff
Joshua Frank <josh@tesseractmobile.com>
2015-06-14 17:48:57 -0500

    Added eye animations

--- src/com/tesseractmobile/efim/views/EyeView.java
+++ src/com/tesseractmobile/efim/views/EyeView.java
@@ -1,6 +1,17 @@
 package com.tesseractmobile.efim.views;
 
+import android.animation.ValueAnimator;
+import android.animation.ValueAnimator.AnimatorUpdateListener;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapFactory.Options;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
 import android.os.Handler;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
@@ -11,9 +22,69 @@ import com.tesseractmobile.efim.R;
 public class EyeView extends View {
     
     private final Handler mHandler = new Handler();
+    private final Bitmap mEyeOpen;
+    private final Rect mEyeRect = new Rect();
+    private final Rect mUpperEyelidRect = new Rect();
+    private final Rect mLowerEyelidRect = new Rect();
+    private final Rect mUpperEyelidRectDest = new Rect();
+    private final Rect mLowerEyelidRectDest = new Rect();
+    private final Rect mUpperEyelidRectSrc = new Rect();
+    private final Rect mLowerEyelidRectSrc = new Rect();
+    private final Canvas mEyeCanvas;
+    private final Paint mEyeLidPaint;
+    private float mAnimationPercent;
+    private final Bitmap mEye;
     
+    @Override
+    protected void onSizeChanged(final int w, final int h, final int oldw, final int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mEyeRect.set(0, 0, w, h);
+        //Start with the eyes closed
+        mUpperEyelidRect.set(0, 0, mEyeCanvas.getWidth(), mEyeCanvas.getHeight() / 2);
+        mLowerEyelidRect.set(0, mEyeCanvas.getHeight() / 2, mEyeCanvas.getWidth(), mEyeCanvas.getHeight());
+        open();
+    }
+
     public EyeView(final Context context, final AttributeSet attrs) {
         super(context, attrs);
+        final BitmapFactory.Options options = new Options();
+        options.inMutable = true;
+        mEyeOpen = BitmapFactory.decodeResource(getResources(), R.drawable.eye_open, options);
+        mEye = BitmapFactory.decodeResource(getResources(), R.drawable.eye_open, options);
+        mEyeCanvas = new Canvas(mEye);
+        //mEyeCanvas.drawColor(Color.RED);
+        mEyeLidPaint = new Paint();
+        mEyeLidPaint.setColor(Color.BLACK);
+        mEyeLidPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_ATOP));
+        
+    }
+
+    /**
+     * Redraws the eye with the current animation settings
+     */
+    private void updateEye() {
+        //Update Top Eyelid
+        final int bottom = updateValue(mUpperEyelidRectSrc.bottom, mUpperEyelidRectDest.bottom, mAnimationPercent);
+        mUpperEyelidRect.set(mUpperEyelidRectDest.left, mUpperEyelidRectDest.top, mUpperEyelidRectDest.right, bottom);
+        //Update Lower EyeLid
+        final int top = updateValue(mLowerEyelidRectSrc.top, mLowerEyelidRectDest.top, mAnimationPercent);
+        mLowerEyelidRect.set(mLowerEyelidRectDest.left, top, mLowerEyelidRectDest.right, mLowerEyelidRectDest.bottom);
+        //Redraw main eye
+        mEyeCanvas.drawBitmap(mEyeOpen, 0, 0, null);
+        //Draw eye lids
+        mEyeCanvas.drawRect(mUpperEyelidRect, mEyeLidPaint);
+        mEyeCanvas.drawRect(mLowerEyelidRect, mEyeLidPaint);
+    }
+ 
+    final int updateValue(final int srcValue, final int dstValue, final float percent){
+        return Math.round(srcValue + (dstValue - srcValue) * percent);
+    }
+    
+    @Override
+    protected void onDraw(final Canvas canvas) {
+        
+        canvas.drawBitmap(mEye, null, mEyeRect, null);
+        //super.onDraw(canvas);
     }
 
     @Override
@@ -21,10 +92,10 @@ public class EyeView extends View {
         final int action = event.getAction();
         switch (action) {
         case MotionEvent.ACTION_DOWN:
-            setBackgroundResource(R.drawable.eye_closing1);
+            squint();
             break;
         case MotionEvent.ACTION_MOVE:
-            setBackgroundResource(R.drawable.eye_closing2);
+            //blink();
             break;
         case MotionEvent.ACTION_UP:
             blink();
@@ -40,29 +111,90 @@ public class EyeView extends View {
      * Must be called from the UI Thread
      */
     public void blink(){
-        //Poor mans animation
-        final int baseSpeed = 100;
-        postDelayed(R.drawable.eye_closing1, 0);
-        final int closeDelay = baseSpeed;
-        postDelayed(R.drawable.eye_closing2, closeDelay);
-        final int secondOpenDelay = closeDelay + baseSpeed * 4;
-        postDelayed(R.drawable.eye_closing1, secondOpenDelay);
-        final int endDelay = secondOpenDelay + baseSpeed;
-        postDelayed(R.drawable.eye_open, endDelay);
+        close();
+        postDelayed(new Runnable() {
+            
+            @Override
+            public void run() {
+                open();
+            }
+        }, 2000);
+    }
+    
+    public void close(){
+        //Save current position
+        saveCurrentEyeLids();
+        //Set destination position
+        mUpperEyelidRectDest.set(0, 0, mEyeCanvas.getWidth(), mEyeCanvas.getHeight() / 2);
+        mLowerEyelidRectDest.set(0, mEyeCanvas.getHeight() / 2, mEyeCanvas.getWidth(), mEyeCanvas.getHeight());
+        //Create animation
+        final ValueAnimator animation = ValueAnimator.ofFloat(0f, 1f);
+        animation.setDuration(1000);
+        animation.addUpdateListener(new AnimatorUpdateListener() {
+            
+            @Override
+            public void onAnimationUpdate(final ValueAnimator animation) {
+                mAnimationPercent = (Float) animation.getAnimatedValue();
+                invalidate();
+            }
+        });
+        animation.start();
+    }
+
+    /**
+     * 
+     */
+    public void saveCurrentEyeLids() {
+        mUpperEyelidRectSrc.set(mUpperEyelidRect);
+        mLowerEyelidRectSrc.set(mLowerEyelidRect);
     }
     
     /**
      * Open the eye
      */
     public void open() {
-        postDelayed(R.drawable.eye_open, 0);
+        //Save current position
+        saveCurrentEyeLids();
+        //Set destination position
+        mUpperEyelidRectDest.set(0, 0, mEyeCanvas.getWidth(), mEyeCanvas.getHeight() / 10);
+        mLowerEyelidRectDest.set(0, (int) (mEyeCanvas.getHeight() * .9), mEyeCanvas.getWidth(), mEyeCanvas.getHeight());
+        final ValueAnimator animation = ValueAnimator.ofFloat(0f, 1f);
+        animation.setDuration(1000);
+        animation.addUpdateListener(new AnimatorUpdateListener() {
+            
+            @Override
+            public void onAnimationUpdate(final ValueAnimator animation) {
+                mAnimationPercent = (Float) animation.getAnimatedValue();
+                invalidate();
+            }
+        });
+        animation.start();
     }
     
     /**
      * Squint the eye
      */
     public void squint() {
-        postDelayed(R.drawable.eye_closing1, 0);
+        saveCurrentEyeLids();
+        mUpperEyelidRectDest.set(0, 0, mEyeCanvas.getWidth(), ((mEyeCanvas.getHeight() / 4)));
+        mLowerEyelidRectDest.set(0, (int) (mEyeCanvas.getHeight() * .9), mEyeCanvas.getWidth(), mEyeCanvas.getHeight());
+        final ValueAnimator animation = ValueAnimator.ofFloat(0f, 1f);
+        animation.setDuration(1000);
+        animation.addUpdateListener(new AnimatorUpdateListener() {
+            
+            @Override
+            public void onAnimationUpdate(final ValueAnimator animation) {
+                mAnimationPercent = (Float) animation.getAnimatedValue();
+                invalidate();
+            }
+        });
+        animation.start();
+    }
+
+    @Override
+    public void invalidate() {
+        updateEye();
+        super.invalidate();
     }
 
     /**
