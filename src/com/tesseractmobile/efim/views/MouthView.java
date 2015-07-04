d2508f35bc4366fd6670fb32e030cc268297cf6c
Joshua Frank <josh@tesseractmobile.com>
2015-07-04 16:30:14 -0500

    Added wave form  to mouth

--- src/com/tesseractmobile/efim/views/MouthView.java
+++ src/com/tesseractmobile/efim/views/MouthView.java
@@ -8,6 +8,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
 import android.graphics.Path;
+import android.graphics.RectF;
 import android.media.audiofx.Visualizer;
 import android.media.audiofx.Visualizer.OnDataCaptureListener;
 import android.os.Handler;
@@ -32,6 +33,8 @@ public class MouthView extends TextView implements OnInitListener, OnDataCapture
     private final Path mPath;
     private final Paint mPaint;
     private final Path mWavePath;
+    private byte[] mWave = new byte[1024];
+    private final Paint mMouthPaint;
     
     public MouthView(final Context context, final AttributeSet attrs) {
         super(context, attrs);
@@ -65,7 +68,8 @@ public class MouthView extends TextView implements OnInitListener, OnDataCapture
         mPaint.setColor(Color.BLACK);
         mPaint.setStrokeWidth(10);
         mPaint.setStyle(Style.STROKE);
-        
+        mMouthPaint = new Paint();
+        mMouthPaint.setColor(Color.WHITE);
 
         final Visualizer mVisualizer = new Visualizer(0);
         mVisualizer.setDataCaptureListener(MouthView.this, Visualizer.getMaxCaptureRate() / 2, true, false);
@@ -75,10 +79,12 @@ public class MouthView extends TextView implements OnInitListener, OnDataCapture
     
     @Override
     protected void onDraw(final Canvas canvas) {
-        canvas.drawColor(Color.WHITE);
         
-
+        drawMouth(getWidth(), getHeight());
+        canvas.drawRoundRect(new RectF(0, 0, canvas.getWidth(), canvas.getHeight()), 50, 50, mMouthPaint);
         canvas.drawPath(mPath, mPaint);
+        canvas.drawPath(mWavePath, mPaint);
+        
         //canvas.drawPath(mWavePath, mPaint);
 //        if(mState == State.TALKING){
 //            final long uptimeMillis = SystemClock.uptimeMillis();
@@ -107,6 +113,7 @@ public class MouthView extends TextView implements OnInitListener, OnDataCapture
      * @param h
      */
     protected void drawMouth(final int w, final int h) {
+        mWavePath.reset();
         mPath.reset();
         mPath.moveTo(0, h / 2);
         //mPath.lineTo(w, h / 2);
@@ -118,6 +125,46 @@ public class MouthView extends TextView implements OnInitListener, OnDataCapture
         for(int line = 1; line < 5; line++){
             mPath.moveTo((w / 5) * line, 0);
             mPath.lineTo((w / 5) * line, h);
+        }
+        
+        int gap = 0;
+        final int waves = 25;
+        final int waveLength = mWave.length / waves;
+        final int waveWidth = w / waves;
+        int centerY = h / 2;
+        mPath.moveTo(0, centerY);
+        mWavePath.moveTo(0, centerY);
+        int x = 0;
+        int sum = 0;
+        int count = 0;
+        int section = 0;
+        for(int i = 0; i < mWave.length; i ++){
+            count++;
+            final byte wave = mWave[i] <= -127 ||  mWave[i] >= 127 ? 0 : mWave[i];
+            sum += wave;
+            if(i % waveLength == 0){
+                section++;
+                if(section > waves / 2){
+                    centerY -= h/2/waves;
+                    gap += 5;
+                } else {
+                    centerY += h/2/waves;
+                    gap -= 5;
+                }
+//                final int radius = h / 2;
+//                final double angle = ((180 / waves) / (2 * Math.PI)) * section;
+//                section++;
+//                centerY = (int) ((h / 2) + radius * Math.cos(angle));
+                final int waveHeight = (sum / count);
+                final int destY = centerY + waveHeight;
+                mPath.quadTo(x + waveWidth / 2, destY + gap, x + waveWidth, centerY + gap);
+                mWavePath.quadTo(x + waveWidth / 2, centerY -waveHeight - gap, x + waveWidth, centerY - gap);
+                //mPath.lineTo(x, destY);
+                x += waveWidth;
+                count = 0;
+                sum = 0;
+            }
+            
         }
     }
 
@@ -204,35 +251,22 @@ public class MouthView extends TextView implements OnInitListener, OnDataCapture
     @Override
     public void onWaveFormDataCapture(final Visualizer visualizer, final byte[] waveform, final int samplingRate) {
         //Log.d("wave", Arrays.toString(waveform));
-        drawMouth(getWidth(), getHeight());
-        final int waveWidth = getWidth() / 10;
-        final int centerY = getHeight() / 2;
-        mPath.moveTo(0, centerY);
-        int x = 0;
-        int sum = 0;
-        int count = 0;
-        for(int i = 0; i < waveform.length && x < getWidth(); i ++){
-            count++;
-            final byte wave = waveform[i];
-            sum += wave;
-            if(i % 100 == 0){
-                //mPath.lineTo(x, getHeight() / 2 + wave);
-                final int waveHeight = sum / count;
-                mPath.quadTo(x + waveWidth / 2, centerY + waveHeight * 2, x + waveWidth, centerY);
-                x += waveWidth;
-                count = 0;
-                sum = 0;
-            }
-            
-        }
+        updateWave(waveform);
+    }
+
+
+    /**
+     * @param waveform
+     */
+    public void updateWave(final byte[] waveform) {
+        mWave = waveform;
         invalidate();
     }
 
 
     @Override
     public void onFftDataCapture(final Visualizer visualizer, final byte[] fft, final int samplingRate) {
-        // TODO Auto-generated method stub
-        
+        updateWave(fft);
     }
     
 }
