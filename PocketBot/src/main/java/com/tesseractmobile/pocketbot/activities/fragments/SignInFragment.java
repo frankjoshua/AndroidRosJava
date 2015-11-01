e007659deb2b70a8432ab1e3e639bf6d45d2cfce
Joshua Frank <josh@tesseractmobile.com>
2015-11-01 17:03:04 -0600

    POC-48 Telepresence WIP

---
+++ PocketBot/src/main/java/com/tesseractmobile/pocketbot/activities/fragments/SignInFragment.java
@@ -0,0 +1,106 @@
+package com.tesseractmobile.pocketbot.activities.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import com.quickblox.auth.QBAuth;
+import com.quickblox.auth.model.QBSession;
+import com.quickblox.core.QBEntityCallbackImpl;
+import com.quickblox.users.QBUsers;
+import com.quickblox.users.model.QBUser;
+import com.tesseractmobile.pocketbot.R;
+import com.tesseractmobile.pocketbot.activities.PocketBotSettings;
+
+import java.util.List;
+
+/**
+ * Created by josh on 11/1/2015.
+ */
+public class SignInFragment extends Fragment implements View.OnClickListener {
+    private EditText mEmail;
+    private EditText mPassword;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.sign_in_layout, null);
+        view.findViewById(R.id.btnSignIn).setOnClickListener(this);
+        mEmail = (EditText) view.findViewById(R.id.etEmail);
+        mPassword = (EditText) view.findViewById(R.id.etPassword);
+        mEmail.setText(PocketBotSettings.getUserName(getActivity()));
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        final String email = mEmail.getText().toString();
+        final String password = mPassword.getText().toString();
+
+        if(TextUtils.isEmpty(password) || password.length() < 8)
+        {
+            mPassword.setError("You must have at least 8 characters in your password");
+            return;
+        }
+
+        final QBUser qbUser = new QBUser(email, password);
+
+        QBAuth.createSession(qbUser, new QBEntityCallbackImpl<QBSession>(){
+            @Override
+            public void onError(List<String> errors) {
+                if(errors.get(0).equals("Unauthorized")){
+                    signUpUser(qbUser);
+                } else {
+                    mEmail.setError(errors.get(0));;
+                }
+            }
+
+            @Override
+            public void onSuccess(QBSession result, Bundle params) {
+                onSuccessfulSignIn();
+            }
+        });
+    }
+
+    private void onSuccessfulSignIn() {
+        //Save user name and password
+        final String email = mEmail.getText().toString();
+        final String password = mPassword.getText().toString();
+        PocketBotSettings.setUserName(getActivity(), email);
+        PocketBotSettings.setPassword(getActivity(), password);
+        //Mark user as signed in
+        PocketBotSettings.setSignedIn(getActivity(), true);
+        //Close fragment
+        getActivity().getSupportFragmentManager().beginTransaction().remove(SignInFragment.this).commit();
+    }
+
+    private void signUpUser(final QBUser qbUser) {
+        QBAuth.createSession(new QBEntityCallbackImpl<QBSession>(){
+
+            @Override
+            public void onError(List<String> errors) {
+                mEmail.setError(errors.get(0));;
+            }
+
+            @Override
+            public void onSuccess(QBSession result, Bundle params) {
+                QBUsers.signUp(qbUser, new QBEntityCallbackImpl<QBUser>(){
+                    @Override
+                    public void onSuccess(QBUser result, Bundle params) {
+                        onSuccessfulSignIn();
+                    }
+
+                    @Override
+                    public void onError(List<String> errors) {
+                        mEmail.setError(errors.get(0));
+                    }
+                });
+            }
+        });
+    }
+}
---
+++ PocketBot/src/main/java/com/tesseractmobile/pocketbot/activities/fragments/TelepresenceFaceFragment.java
@@ -0,0 +1,113 @@
+package com.tesseractmobile.pocketbot.activities.fragments;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.quickblox.auth.QBAuth;
+import com.quickblox.auth.model.QBSession;
+import com.quickblox.chat.QBChatService;
+import com.quickblox.chat.QBSignaling;
+import com.quickblox.chat.QBWebRTCSignaling;
+import com.quickblox.chat.listeners.QBVideoChatSignalingManagerListener;
+import com.quickblox.core.QBEntityCallbackImpl;
+import com.quickblox.core.QBSettings;
+import com.quickblox.users.QBUsers;
+import com.quickblox.users.model.QBUser;
+import com.quickblox.videochat.webrtc.QBRTCClient;
+import com.quickblox.videochat.webrtc.QBRTCException;
+import com.quickblox.videochat.webrtc.QBRTCSession;
+import com.quickblox.videochat.webrtc.callbacks.QBRTCClientConnectionCallbacks;
+import com.quickblox.videochat.webrtc.callbacks.QBRTCClientSessionCallbacks;
+import com.quickblox.videochat.webrtc.callbacks.QBRTCClientVideoTracksCallbacks;
+import com.quickblox.videochat.webrtc.view.QBGLVideoView;
+import com.quickblox.videochat.webrtc.view.QBRTCVideoTrack;
+import com.quickblox.videochat.webrtc.view.VideoCallBacks;
+import com.tesseractmobile.pocketbot.R;
+import com.tesseractmobile.pocketbot.activities.PocketBotSettings;
+import com.tesseractmobile.pocketbot.robot.faces.ControlFace;
+import com.tesseractmobile.pocketbot.robot.faces.RobotFace;
+import com.tesseractmobile.pocketbot.robot.faces.RobotInterface;
+import com.tesseractmobile.pocketbot.robot.faces.TelePresenceFace;
+
+import org.webrtc.VideoRenderer;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by josh on 10/31/2015.
+ */
+public class TelepresenceFaceFragment extends QuickBloxFragment {
+
+
+    private RobotFace mRobotFace;
+    private QBGLVideoView mRemoteVideoView;
+    private TextView mUserId;
+
+    @Override
+    public RobotFace getRobotFace(RobotInterface robotInterface) {
+        mRobotFace.setRobotInterface(robotInterface);
+        return mRobotFace;
+    }
+
+    @Override
+    protected View createView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.face_telepresence, null);
+        mRobotFace = new TelePresenceFace(view);
+        mRemoteVideoView = (QBGLVideoView) view.findViewById(R.id.remoteVideoView);
+        mUserId = (TextView) view.findViewById(R.id.tvUserId);
+        return view;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //QBSettings.getInstance().fastConfigInit("92", "wJHdOcQSxXQGWx5", "BTFsj7Rtt27DAmT"); //Test
+
+    }
+
+    @Override
+    protected void onQBSetup(final QBSession session, final QBUser user) {
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mUserId.setText(Integer.toString(session.getUserId()));
+            }
+        });
+    }
+
+    @Override
+    public void onReceiveNewSession(final QBRTCSession qbrtcSession) {
+        // Set userInfo
+        // User can set any string key and value in user info
+        // Then retrieve this data from sessions which is returned in callbacks
+        // and parse them as he wish
+        final Map<String,String> userInfo = new HashMap<String,String>();
+        userInfo.put("Key", "Value");
+
+        // Accept incoming call
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                qbrtcSession.acceptCall(userInfo);
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onRemoteVideoTrackReceive(QBRTCSession qbrtcSession, QBRTCVideoTrack qbrtcVideoTrack, Integer integer) {
+        //Setup Remote video
+        VideoRenderer remoteRenderer = new VideoRenderer(new VideoCallBacks(mRemoteVideoView, QBGLVideoView.Endpoint.REMOTE));
+        qbrtcVideoTrack.addRenderer(remoteRenderer);
+        mRemoteVideoView.setVideoTrack(qbrtcVideoTrack, QBGLVideoView.Endpoint.REMOTE);
+    }
+}
