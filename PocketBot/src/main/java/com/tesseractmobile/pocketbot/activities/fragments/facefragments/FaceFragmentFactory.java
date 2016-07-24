d9e903c710d518c33464c31fa82f689b80b750d2
Joshua Frank <josh@tesseractmobile.com>
2016-07-24 02:38:40 -0500

    Added face icons

--- PocketBot/src/main/java/com/tesseractmobile/pocketbot/activities/fragments/facefragments/FaceFragmentFactory.java
+++ PocketBot/src/main/java/com/tesseractmobile/pocketbot/activities/fragments/facefragments/FaceFragmentFactory.java
@@ -47,8 +47,8 @@ public class FaceFragmentFactory {
     public static List<FaceInfo> getFaceInfoList() {
         final ArrayList<FaceInfo> faceList = new ArrayList<>();
         faceList.add(new FaceInfo(ID_FACE_EFIM, R.drawable.ic_launcher, R.drawable.efim_background_texture, "Robot", "Face tracking is active and AI is in total control.", false));
-        faceList.add(new FaceInfo(ID_FACE_ALIEN, R.drawable.ic_launcher, R.drawable.purple_fuzz, "Fuzzy", "Face tracking is active and AI is in total control.", false));
-        faceList.add(new FaceInfo(ID_FACE_NEW, R.drawable.ic_launcher, R.drawable.purple_fuzz, "Scratchy", "Face tracking is active and AI is in total control.", false));
+        faceList.add(new FaceInfo(ID_FACE_ALIEN, R.drawable.ic_face_fuzzy, R.drawable.purple_fuzz, "Fuzzy", "Face tracking is active and AI is in total control.", false));
+        faceList.add(new FaceInfo(ID_FACE_NEW, R.drawable.ic_face_scratchy, R.drawable.purple_fuzz, "Scratchy", "Face tracking is active and AI is in total control.", false));
         faceList.add(new FaceInfo(ID_FACE_CONTROL, R.drawable.ic_control, R.drawable.efim_background_texture, "Control", "Used for direct control of remote or local robot.", false));
         faceList.add(new FaceInfo(ID_FACE_TELEPRESENCE, R.drawable.ic_telepresence, R.drawable.efim_background_texture, "Telepresence", "Allow remote control and show remote video feed on screen.", false));
         faceList.add(new FaceInfo(ID_FACE_TELEPRESENCE_EFIM, R.drawable.ic_robot_telepresence, R.drawable.efim_background_texture, "Remote Robot", "Telepresence while displaying robot face instead of video.", false));
