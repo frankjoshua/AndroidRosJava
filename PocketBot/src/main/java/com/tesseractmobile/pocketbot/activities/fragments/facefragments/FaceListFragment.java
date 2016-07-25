349b46265446139de28a52b281b1741e685097d2
Joshua Frank <josh@tesseractmobile.com>
2016-07-25 17:25:40 -0500

    POC-134 Fixed slow drawer

--- PocketBot/src/main/java/com/tesseractmobile/pocketbot/activities/fragments/facefragments/FaceListFragment.java
+++ PocketBot/src/main/java/com/tesseractmobile/pocketbot/activities/fragments/facefragments/FaceListFragment.java
@@ -5,6 +5,8 @@ import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,66 +25,80 @@ import java.util.List;
 /**
  * Created by josh on 1/24/2016.
  */
-public class FaceListFragment extends Fragment implements AdapterView.OnItemClickListener {
-
-    private ListView listView;
+public class FaceListFragment extends Fragment{
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.face_selector, container);
-        listView = (ListView) view.findViewById(R.id.lvFaces);
-        //Add all emotions to the list view
-        final ArrayAdapter<FaceInfo> adapter = new FaceArrayAdapter<FaceInfo>(getActivity(), android.R.layout.simple_list_item_1, FaceFragmentFactory.getFaceInfoList());
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(this);
+        final RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.rvFaces);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setHasFixedSize(false);
+
+        recyclerView.setAdapter(new FaceListAdapter());
+
         return view;
     }
 
 
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Activity activity = getActivity();
-        if(activity != null) {
-            final FaceInfo faceInfo = (FaceInfo) listView.getItemAtPosition(position);
-            PocketBotSettings.setSelectedFace(activity, faceInfo.id);
-        }
-    }
 
-    private class FaceArrayAdapter<T> extends ArrayAdapter<FaceInfo> {
-        public FaceArrayAdapter(final Activity activity, final int simple_list_item_1, final List<FaceInfo> faceInfoList) {
-            super(activity, simple_list_item_1, faceInfoList);
+    private class FaceListAdapter extends RecyclerView.Adapter<FaceListAdapter.ViewHolder>{
+
+        private List<FaceInfo> faceInfoList = FaceFragmentFactory.getFaceInfoList();
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            //Inflate the view
+            final View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.face_list_item, parent, false);
+            return new ViewHolder(view);
         }
 
         @Override
-        public View getView(final int position, final View convertView, final ViewGroup parent) {
-            final View view;
-            final LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            if(convertView != null){
-                view = convertView;
-            } else {
-                view = inflater.inflate(R.layout.face_list_item, null);
-            }
-            final TextView tvName = (TextView) view.findViewById(R.id.tvName);
-            final TextView tvInfo = (TextView) view.findViewById(R.id.tvInfo);
-            final ImageView ivIcon = (ImageView) view.findViewById(R.id.ivFaceIcon);
-            final LinearLayout llLock = (LinearLayout) view.findViewById(R.id.llLock);
-            final FaceInfo faceInfo = (FaceInfo) listView.getItemAtPosition(position);
-            //view.setBackgroundResource(faceInfo.background);
-            tvName.setText(faceInfo.name);
-            tvInfo.setText(faceInfo.info);
-            ivIcon.setImageResource(faceInfo.icon);
-
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            final FaceInfo faceInfo = faceInfoList.get(position);
+            holder.tvName.setText(faceInfo.name);
+            holder.tvInfo.setText(faceInfo.info);
+            holder.ivIcon.setImageResource(faceInfo.icon);
+            //holder.ivIcon.setVisibility(View.INVISIBLE);
+            //Lock if needed
             if(faceInfo.locked){
-                llLock.setVisibility(View.VISIBLE);
+                holder.llLock.setVisibility(View.VISIBLE);
             } else {
-                llLock.setVisibility(View.GONE);
+                holder.llLock.setVisibility(View.GONE);
+                holder.view.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        final Activity activity = getActivity();
+                        if(activity != null){
+                            PocketBotSettings.setSelectedFace(activity, faceInfo.id);
+                        }
+                    }
+                });
             }
+        }
 
-//            final ImageView imageView = (ImageView) view.findViewById(R.id.ivBackground);
-//            imageView.setImageResource(faceInfo.background);
+        @Override
+        public int getItemCount() {
+            faceInfoList = FaceFragmentFactory.getFaceInfoList();
+            return faceInfoList.size();
+        }
 
-            return view;
+        public class ViewHolder extends RecyclerView.ViewHolder{
+            final TextView tvName;
+            final TextView tvInfo;
+            final ImageView ivIcon;
+            final LinearLayout llLock;
+            final View view;
+
+            public ViewHolder(final View view) {
+                super(view);
+                this.view = view;
+                tvName = (TextView) view.findViewById(R.id.tvName);
+                tvInfo = (TextView) view.findViewById(R.id.tvInfo);
+                ivIcon = (ImageView) view.findViewById(R.id.ivFaceIcon);
+                llLock = (LinearLayout) view.findViewById(R.id.llLock);
+            }
         }
     }
+
 }
